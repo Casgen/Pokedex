4b2c41d905ea2f2b85d99d77b577148716270924
Casgen <vzotov42@gmail.com>
2018-05-06 19:20:18 +0200

    Add files via upload

--- Pokedex_Zotov-Merta/php/catchem.php
+++ Pokedex_Zotov-Merta/php/catchem.php
@@ -6,9 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/pokedex.css">
+    <link rel="stylesheet" href="../css/catchem.css">
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i|IBM+Plex+Sans+Condensed:400,400i|IBM+Plex+Sans:100,100i,400,400i,700,700i|IBM+Plex+Serif:400,400i" rel="stylesheet">
-    <title>Vítejte v Pokedexu!</title>
+    <title>Chytni si Pokémona!</title>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
@@ -18,13 +18,13 @@
     <script src="../js/jquery-3.3.1.min.js"></script>
 </head>
 <?php
-        $styl = "<span class='glyphicon glyphicon-search'></span>";
+        
         include 'server.php';
         if(isset($_SESSION["id"])){ 
-        $jmeno= $_SESSION["jmeno"];
+        $jmeno = $_SESSION["jmeno"];
         }
 ?>
-<body style="background-color:#7fad71">
+<body style="background-color:rgb(63, 198, 247);">
     
     
     <!-- porovnat kody od zbyńdose a přidat do mého kódu jeho upravený -->    
@@ -42,45 +42,11 @@
     
     
     
-    <header style="line-height:2;border:none;background-size:cover;" class="divy">
-        <h1 id="uvodtext">Vítáme vás v Pokédexu!</h1>
+    <header style="line-height:2;background-size:cover;">
+        <h1 id="uvodtext">Gotta Catch em All!</h1>
     </header>
     <div id="zobrazeni">
-        <nav class="divy" id="pokemonnavigate">
-
-            <!--Vytvoření-->
-            <a href="createpokemon.php">
-                <button style="Width:100%;margin-bottom:10px;background-color:green;border:1px solid green;" class="btn btn-primary" type="button" aria-expanded="false" aria-controls="collapseExample">
-                    Vytvoř si Pokémona!
-                </button>
-            </a>
-            
-            
-            <!--typy-->
-             <button style="Width:100%;margin-bottom:10px;background-color:rgb(0, 120, 255);border:1px solid rgb(0, 120, 255)"
-             class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseTypy" aria-expanded="false" aria-controls="collapseExample">Typy</button>
-            
-            <div class="collapse" id="collapseTypy">
-              <div class="card card-body">
-                Typ1
-              </div>
-            </div>
-
-            <!--Slabosti-->
-             <button style="Width:100%;margin-bottom:10px;background-color:red;border:1px solid red"
-             class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseSlabosti" aria-expanded="false" aria-controls="collapseExample">
-                Slabosti
-            </button>
-            
-            <div class="collapse" id="collapseSlabosti">
-              <div class="card card-body">
-                Slabost 1
-              </div>
-            </div>
-        
-        </nav>
         <article class="divy">
-            <p id="pokemonHead">Pokémoni</p>
             <div style="display:flex;flex-direction:row;">
                 <input type="submit" value="Vyhledat" style="height:40px;margin-left:auto;margin-right:auto;" class="btn btn-info">
                 <input style="width:60%;margin:auto;margin-bottom:40px;" type="search" placeholder="Hledej Pokemony..." name="" id="input${1/(\w+)/\u\1/g}" class="form-control" value="" required="required" title="">
@@ -92,7 +58,7 @@
                         $dotaz->execute();
         echo"<div class='flex-container'>";              
         while($dotaz->fetch()){      
-        echo "<div class='obrazky'><a href='detail.php?id=$id'><img class='obrPokemona' src='$obrazek' height='200px' width='200px'><h3  class='nazevPokemona'>Text</h3></a></div>";
+        echo "<div class='obrazky'><a href='detail.php?id=$id'><img class='obrPokemona' src='$obrazek' height='200px' width='200px'><h3  class='nazevPokemona'>Text<br>Chytit</h3></a></div>";
             }
         echo "</div>";
         ?>
