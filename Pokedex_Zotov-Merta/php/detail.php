0cd60948207b96205f14c3df19d363e64ad4bca0
Casgen <vzotov42@gmail.com>
2018-05-07 23:11:21 +0200

    Add files via upload

--- Pokedex_Zotov-Merta/php/detail.php
+++ Pokedex_Zotov-Merta/php/detail.php
@@ -8,10 +8,10 @@ if(!isset($_SESSION["id"])) header("Location: ../index.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Název Pokémona</title>
+    <title>Pokémon</title>
+    <link rel="icon" href="../images/ikona.png">
     <link rel="stylesheet" href="../css/detail.css">
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i|IBM+Plex+Sans+Condensed:400,400i|IBM+Plex+Sans:100,100i,400,400i,700,700i|IBM+Plex+Serif:400,400i" rel="stylesheet">
-    <title>Vítejte v Pokedexu!</title>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
@@ -32,10 +32,9 @@ if(!isset($_SESSION["id"])) header("Location: ../index.php");
         $dotaz->fetch();
         $dotaz->close();
         ?>
-<body style="background-color:rgb(40, 199, 0)">
+<body style="background-image: linear-gradient(90deg, rgb(247, 247, 247), rgb(208, 223, 255), rgb(247, 247, 247));">
     <header><h1 id="nazevPokemona"><?php echo "$nazev";?></h1></header>
     <div style="display:flex;flex-direction:row;" id="prostredek">
-        <div class="strany"></div>
         <article>
             <div id="obsah">
                 <img id="obrPokemona" src="<?php echo "$obrazek";?>">
@@ -82,7 +81,6 @@ if(!isset($_SESSION["id"])) header("Location: ../index.php");
                     </div>
             </div>
         </article>
-        <div class="strany"></div>
     </div>
 </body>
 </html>
